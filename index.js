23d5d8b0ad2c9cfe3f666a27fae03d3ea62cea08
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-10-18 17:25:47 -0500

    added write to file function in index.js

--- index.js
+++ index.js
@@ -4,7 +4,33 @@ const fs = require("fs");
 
 const { Triangle, Square, Circle } = require("./lib/shapes");
 
+function writeToFile(fileName, answers) {
+    
+    let svgString = "";
 
+    svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svgString += "<g>";
+    svgString += `${answers.shape}`;
+
+    let shapeChoice;
+    if (answers.shape === "Triangle") {
+        shapeChoice = new Triangle();
+        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.logoColor}"/>`;
+    }   else if (answers.shape === "Square") {
+        shapeChoice = new Square();
+        svgString += `<rect x="73" y="40" width="160" fill="${answers.logoColor}"/>`;
+    }   else {
+        shapeChoice += `<circle cx="150" cy="115" r="80" fill="${answers.logoColor}">`;
+    }
+
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}"/>`;
+    svgString += "</g>";
+    svgString += "</svg>";
+
+    fs.writeFile(fileName, svgString, (err) => {
+        err ? console.log(err) : console.log("Generated logo.svg");
+    });
+} 
 
 const questions = [
     {
