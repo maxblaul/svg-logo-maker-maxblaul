8a7f1917ccfc7b019db3ba4b8b5ccabedb9c0708
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-10-18 19:00:03 -0500

    fixing some errors

--- index.js
+++ index.js
@@ -20,10 +20,12 @@ function writeToFile(fileName, answers) {
         shapeChoice = new Square();
         svgString += `<rect x="73" y="40" width="160" fill="${answers.logoColor}"/>`;
     }   else {
-        shapeChoice += `<circle cx="150" cy="115" r="80" fill="${answers.logoColor}">`;
+        shapeChoice = new Circle();
+        svgString += `<circle cx="150" cy="115" r="80" fill="${answers.logoColor}">`;
     }
 
-    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}"/>`;
+    svgString += `<text x="150" y="150" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.logoName}</text>`;
+
     svgString += "</g>";
     svgString += "</svg>";
 
@@ -58,26 +60,13 @@ function promptUser() {
     },  
 ])
 .then((answers) => {
-    if (answers.text.length > 3) {
+    if (answers.logoName.length > 3) {
         console.log("Please enter no more than 3 characters");
         promptUser();
     }  else {
         writeToFile("logo.svg", answers);
     }
-});
+ });
 }
         
-
-// function writeToFile(fileName, data) {
-//     return fs.writeFileSync(path.join(process.cwd(), fileName), data)
-// }
-
-// function init() {
-
-//     inquirer.createPromptModule(questions).then((responses) => {
-//         console.log("Making the SVG Logo");
-//         writeToFile('samplelogo.svg', generateLogo({...response}))
-//     })
-// }
-
-init();
+promptUser();
